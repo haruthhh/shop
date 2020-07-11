2d60584d082424b4126a16bbcb3e8f090d6cb60a
haruthhh <haruthhh@mail.ru>
2020-07-11 21:39:33 +0400

    small changes

--- admin/controller/adminController.php
+++ admin/controller/adminController.php
@@ -1,5 +1,7 @@
 <?php
+
 session_start();
+
 require_once('../model/model.php');
 if(!isset($_POST['action']))die;
 if(empty(trim($_POST['login']))||empty(trim($_POST['password']))){
@@ -7,13 +9,14 @@ if(empty(trim($_POST['login']))||empty(trim($_POST['password']))){
 	header("location:../index.php");
 	die;
 }
+
 $login=$_POST['login'];
 $password=$_POST['password'];
-if(checkAdmin($login,$password)){
+
+if(checkAdmin($login,$password)) {
 	header("location:../view/home.php");	
-}
-else{
-	$_SESSION['error']="Incorrect login or password!";
+} else {
+	$_SESSION['error'] = "Incorrect login or password!";
 	header("location:../index.php");
 	die;
 }
